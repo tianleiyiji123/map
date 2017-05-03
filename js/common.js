8b06642efa8f5fecac62347e97af45230fcf0613
tianleiyiji123 <1094017573@qq.com>
2017-05-03 11:28:07 +0800

    框架出来

---
+++ js/common.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Lwang on 2017-05-03.
+ */
+$(function () {
+    $("#shou").click(function () {
+        $("#J-info").animate({
+            left: -350
+        }, 600, "linear", function () {
+            $("#fang").css("display", "block");
+            $("#shou").css("display", "none");
+            $(".tmi-position").css("height", 0)
+        })
+    });
+    $("#fang").click(function () {
+        $(".tmi-position").css("height", 700)
+        $("#J-info").animate({
+            left: 0
+        }, 600, "linear", function () {
+            $("#fang").css("display", "none");
+            $("#shou").css("display", "block");
+        })
+    })
+});
